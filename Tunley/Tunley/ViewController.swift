4e784a598e09fec9e0df3172e47977b224a6bc2e
angel zambrano <angeldzambrano99@gmail.com>
2023-02-28 09:33:28 -0800

    added the set up for no storyboard

--- Tunley/Tunley/ViewController.swift
+++ Tunley/Tunley/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK:  properties
+    let tableview: UITableView = {
+           let tb = UITableView()
+           tb.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+            tb.backgroundColor = .red
+//           tb.register(RestaurantTableViewCell.self, forCellReuseIdentifier: RestaurantTableViewCell.identifier)
+           return tb
+       }()
+       
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        // set up the tableview
+        
+        view.addSubview(tableview)
+        tableview.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableview.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableview.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
     }
 
 
 }
+
+
 
